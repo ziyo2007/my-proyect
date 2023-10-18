60da9b8e3464887b1352fd9c1d22dc9378018680
ziyo2007 <qodirjonovz6@gmail.com>
2023-10-18 21:11:46 +0300

    sdsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <About/>
+      <Footer/>
     </div>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,8 +6,11 @@ function About() {
     <div className='about'>
         <div className="container">
             <div className="about__wrapper">
-            <div className="about__left">
-                <h2 className='about-tittle'>ABOUT ME</h2>
+            <div className="about__left">   
+                  <h2 className='about-tittle'>
+                  <span className='about-span'>ABOUT</span>
+                  <span className='about-span'>ME</span>
+                </h2>
                 <h1 className='about__tittle'>Qodirjonov Ziyodillo</h1>
                 <p className='info-about-me'>
                 My full name is Qodirjonov Ziyodillo. <br />
@@ -15,10 +18,42 @@ function About() {
                 I am also interested in being a doctor and am even studying. <br /> In the future I want to become a good doctor and programmer.</p>
             </div>
             <div className="about__right">
-<img className='me-img' src={imgMe} alt="img" />
+              <img className='me-img' src={imgMe} alt="img" />
             </div>
             </div>
         </div>
+        {/* <ul>
+    <li>
+      <input type="checkbox" />
+      <div>W</div>
+    </li>
+    <li>
+      <input type="checkbox" />
+      <div>E</div>
+    </li>
+    <li>
+      <input type="checkbox" />
+      <div>B</div>
+    </li>
+  </ul>
+  <ul>
+    <li>
+      <input type="checkbox" />
+      <div>B</div>
+    </li>
+    <li>
+      <input type="checkbox" />
+      <div>E</div>
+    </li>
+    <li>
+      <input type="checkbox" />
+      <div>K</div>
+    </li>
+    <li>
+      <input type="checkbox" />
+      <div>S</div>
+    </li>
+  </ul> */}
     </div>
   )
 }
