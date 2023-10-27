1f274fc966f585a3c7772f8c2b347b19b1b3f7d3
ziyo2007 <qodirjonovz6@gmail.com>
2023-10-27 15:59:52 +0500

    dff

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './Portfolio.scss'
 import firstPort from '../../img/first-port.jpg'
 import secondPort from '../../img/second-port.jpg'
-import thirdPort from '../../img/third_port.jpg'
+// import thirdPort from '../../img/third_port.jpg'
+import thirdPort from '../../img/third-port.jpg'
 
 
 function Portfolio() {
@@ -10,34 +11,30 @@ function Portfolio() {
     <div id='portfolio' className='portfolio'> 
       <div className="container">
         <div className="wrapper">
-            <ul className='portfolio-list'>
-                <li className='portfolio__list-item'>
-                    <img className='first-img' src={firstPort} alt="" />
-                    <h2>salom</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                         Facere quo fugiat hic aperiam! Voluptas esse, doloremque 
-                         deleniti eligendi ullam quisquam voluptatum rem, cum 
-                         saepe maxime totam, autem aut minus nulla!</p>
-                         <button>Read more</button>
-                </li>
-                <li className='portfolio__list-item'>
-                    <img className='sec-img' src={secondPort} alt="" />
-                    <h2>salom</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex odit atque mollitia. 
-                        Fugit rem architecto alias ullam, repellat doloribus voluptat
-                        e, similique vel fuga mollitia minima esse, consequuntur necessitatibus ipsa odit?</p>
-                    <button>Read more</button>
-                </li>
-                <li className='portfolio__list-item'>
-                    <img className='third-img' src={thirdPort} alt="" />
-                    <h2>salom</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                        Aliquid officia praesentium possimus expedita iure,
-                         voluptatem quam libero, consequatur vitae temporibus, eaque repudiandae
-                         repellat? Maxime minus blanditiis expedita reiciendis ea rerum?</p>
-                         <button>Read more</button>
-                </li>
-            </ul>
+            <div className="card1">
+              <img className='port-img1' src={firstPort} alt="" />
+              <div className="card1-body">
+                <h2 className='card1-tittle'>proyect</h2>
+                <p className='card1-info'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aspernatur, corporis voluptas accusamus tempore necessitatibus labore, error quod exercitationem dolorem ut accusantium? Sed, necessitatibus consequuntur?</p>
+                <a href="https://f3-team-project3.vercel.app/ " target='_blank'><button className='card1-btn'>Go to proyect</button></a>
+              </div>
+            </div>
+            <div className="card2">
+              <img className='port-img2' src={secondPort} alt="" />
+              <div className="card2-body">
+                <h2 className='card2-tittle'>proyect</h2>
+                <p className='card2-info'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aspernatur, corporis voluptas accusamus tempore necessitatibus labore, error quod exercitationem dolorem ut accusantium? Sed, necessitatibus consequuntur?</p>
+                <a href="https://f3-badiat-proyekt.vercel.app/ " target='_blank'><button className='card2-btn'>Go to proyect</button></a>
+              </div>
+            </div>
+            <div className="card3">
+              <img className='port-img3' src={thirdPort} alt="" />
+              <div className="card3-body">
+                <h2 className='card3-tittle'>proyect</h2>
+                <p className='card3-'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aspernatur, corporis voluptas accusamus tempore necessitatibus labore, error quod exercitationem dolorem ut accusantium? Sed, necessitatibus consequuntur?</p>
+                <a href="https://portifolio-first-project.vercel.app/ " target='_blank'><button className='card3-btn'>Go to proyect</button></a>
+              </div>
+            </div>
         </div>
       </div>
     </div>
